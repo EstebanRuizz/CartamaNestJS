7d708d106a8915c3897b80dabd9b4930318bf416
Maritza Ruiz Ospina <maritzaruizospina@gmail.com>
2023-07-13 23:25:57 -0500

    Type publications

---
+++ Parque.Application/Features/TypePublications/Commands/CreateTypePublications/CreateTypePublicationsCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parque.Application.Features.TypePublications.Commands.CreateTypePublications
+{
+    internal class CreateTypePublicationsCommand
+    {
+    }
+}
