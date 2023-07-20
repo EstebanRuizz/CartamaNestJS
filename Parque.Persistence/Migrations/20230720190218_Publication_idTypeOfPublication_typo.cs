4e1ea1f616f0c9b2375d168448f50e38ca959c60
Esteban Ruiz <rubianruiz05@my.com>
2023-07-20 14:07:28 -0500

    Solved Typo at Publication POST

---
+++ Parque.Persistence/Migrations/20230720190218_Publication_idTypeOfPublication_typo.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parque.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Publication_idTypeOfPublication_typo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Publication_TypePublications_IdTypeOfPulblication",
+                table: "Publication");
+
+            migrationBuilder.RenameColumn(
+                name: "IdTypeOfPulblication",
+                table: "Publication",
+                newName: "idTypeOfPublication");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Publication_IdTypeOfPulblication",
+                table: "Publication",
+                newName: "IX_Publication_idTypeOfPublication");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Publication_TypePublications_idTypeOfPublication",
+                table: "Publication",
+                column: "idTypeOfPublication",
+                principalTable: "TypePublications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Publication_TypePublications_idTypeOfPublication",
+                table: "Publication");
+
+            migrationBuilder.RenameColumn(
+                name: "idTypeOfPublication",
+                table: "Publication",
+                newName: "IdTypeOfPulblication");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Publication_idTypeOfPublication",
+                table: "Publication",
+                newName: "IX_Publication_IdTypeOfPulblication");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Publication_TypePublications_IdTypeOfPulblication",
+                table: "Publication",
+                column: "IdTypeOfPulblication",
+                principalTable: "TypePublications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
